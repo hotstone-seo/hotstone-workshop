7027ef1b63ee6635dbc5f1b6c87eba38f18845d3
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-06-10 17:04:39 +0700

    setup minimal ssr example

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,52 @@
 import path from 'path';
 import express from 'express';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import ReactDOMServer from 'react-dom/server';
+import serialize from 'serialize-javascript';
+import App from '../App';
 
-import htmlMiddleware from './middleware/html';
-import renderMiddleware from './middleware/render';
+// import htmlMiddleware from './middleware/html';
+// import renderMiddleware from './middleware/render';
 
 const publicPath = path.join(__dirname, '/public');
 const app = express();
 
 app.use(express.static(publicPath));
-app.use(htmlMiddleware());
-app.use(renderMiddleware());
+// app.use(htmlMiddleware());
+// app.use(renderMiddleware());
+
+const template = ({ body, helmet }, data) => {
+    return `
+      <!DOCTYPE html>
+      <html ${helmet.htmlAttributes.toString()}>
+        <head>
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+          ${helmet.link.toString()}
+          ${helmet.script.toString()}
+        </head>
+        <body ${helmet.bodyAttributes.toString()}>
+          <div id="root">${body}</div>
+          <script>window.__data = ${serialize(data)}</script>
+        </body>
+      </html>
+    `
+}
+
+app.get('*', (req, res, next) => {
+    (async function() {
+      try {
+        const data = {}
+
+        const appString = ReactDOMServer.renderToString(<App />);
+        const helmet = Helmet.renderStatic();
+   
+        res.send(template({ body: appString, helmet: helmet }, data));
+      } catch(error) {
+        next(error);
+      }
+    })();
+   });
 
 export default app;
